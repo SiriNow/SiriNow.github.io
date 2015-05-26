b3d6dbe266cc6640c7326dd68a9fc499329b73e9
SiriNow <bestibetgroup@gmail.com>
2015-05-25 21:34:40 -0400

    Angry Birds

--- web.sh
+++
@@ -1,7 +0,0 @@
-chmod a+x /app/.heroku/python/lib/python2.7/site-packages/speech_recognition/flac-linux-i386
-if [ "$DEV" = "true" ]
-then
-  python app.py
-else
-  gunicorn -b "0.0.0.0:$PORT" app:app
-fi
